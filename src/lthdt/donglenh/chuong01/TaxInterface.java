27725f0bc480e9bcd92ddd5d0433fee5329c73b4
Phuong <phuongtranpt0606@gmail.com>
2021-03-01 17:16:35 +0700

    Bai 08: Interface (giao dien)

---
+++ src/lthdt/donglenh/chuong01/TaxInterface.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lthdt.donglenh.chuong01;
+
+/**
+ *
+ * @author LENOVO
+ */
+public interface TaxInterface {
+    public double calcTax(double income);
+}
